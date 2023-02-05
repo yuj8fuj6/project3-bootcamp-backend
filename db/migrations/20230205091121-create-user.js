f7d3bf9357210002804cf5afba1666479ea043dc
yuj8fuj6 <yuj.m.fuj91@gmail.com>
2023-02-05 17:17:57 +0800

    Done base sequelize config, user table

---
+++ db/migrations/20230205091121-create-user.js
@@ -0,0 +1,43 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      first_name: {
+        type: Sequelize.TEXT
+      },
+      last_name: {
+        type: Sequelize.TEXT
+      },
+      phone_number: {
+        type: Sequelize.INTEGER
+      },
+      email_address: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      profile_pic_url: {
+        type: Sequelize.STRING
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('users');
+  }
+};
