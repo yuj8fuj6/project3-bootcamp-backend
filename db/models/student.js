644766c2b641c8e24d1fd00b1af76ce87aef731e
yuj8fuj6 <yuj.m.fuj91@gmail.com>
2023-02-07 21:32:16 +0800

    Done student table 1-1 relation with user table

---
+++ db/models/student.js
@@ -0,0 +1,36 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Student extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      this.belongsTo(models.user);
+    }
+  }
+  Student.init(
+    {
+      user_id: {
+        type: DataTypes.INTEGER,
+        references: { model: "users", key: "id" },
+      },
+      total_unit: DataTypes.INTEGER,
+      completed_unit: DataTypes.INTEGER,
+      outstanding_unit: DataTypes.INTEGER,
+      yearly_unit: DataTypes.INTEGER,
+      degree: DataTypes.STRING,
+      school: DataTypes.STRING,
+      academic_year: DataTypes.INTEGER,
+      gpa: DataTypes.DECIMAL,
+    },
+    {
+      sequelize,
+      modelName: "student",
+      underscored: true,
+    },
+  );
+  return Student;
+};
--- db/models/user.js
+++ db/models/user.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      this.hasOne(models.student);
     }
   }
   User.init(
