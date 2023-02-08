28219c7d2db889f891c83ca86cb66e419d26f176
yuj8fuj6 <yuj.m.fuj91@gmail.com>
2023-02-09 00:35:25 +0800

    Slimmed down the migrations

---
+++ db/migrations/20230207085551-secondary-tables.js
@@ -0,0 +1,116 @@
+"use strict";
+
+const { DataTypes } = require("sequelize");
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("students", {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: DataTypes.UUID,
+        defaultValue: Sequelize.literal("gen_random_uuid()"),
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "users",
+          key: "id",
+        },
+      },
+      total_unit: {
+        type: Sequelize.INTEGER,
+      },
+      completed_unit: {
+        type: Sequelize.INTEGER,
+      },
+      outstanding_unit: {
+        type: Sequelize.INTEGER,
+      },
+      yearly_unit: {
+        type: Sequelize.INTEGER,
+      },
+      degree: {
+        type: Sequelize.STRING,
+      },
+      school: {
+        type: Sequelize.STRING,
+      },
+      academic_year: {
+        type: Sequelize.INTEGER,
+      },
+      gpa: {
+        type: Sequelize.DECIMAL,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+    await queryInterface.createTable("professors", {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: DataTypes.UUID,
+        defaultValue: Sequelize.literal("gen_random_uuid()"),
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "users",
+          key: "id",
+        },
+      },
+      admin_permission: {
+        type: Sequelize.BOOLEAN,
+      },
+      school: {
+        type: Sequelize.STRING,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+    await queryInterface.createTable("admins", {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: DataTypes.UUID,
+        defaultValue: Sequelize.literal("gen_random_uuid()"),
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "users",
+          key: "id",
+        },
+      },
+      admin_permission: {
+        type: Sequelize.BOOLEAN,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("students");
+    await queryInterface.dropTable("professors");
+    await queryInterface.dropTable("admins");
+  },
+};
