15e94cf2da2fba2a4b6fd4d6c466ba07ff9cdd01
Nicklye26 <nicklye26@gmail.com>
2023-02-17 16:27:37 +0800

    socket connection working

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const cors = require("cors");
 const express = require("express");
 require("dotenv").config();
 
+const http = require("http");
+const { Server } = require("socket.io");
+
 // importing Routers
 const UsersRouter = require("./routers/usersRouter");
 const CoursesRouter = require("./routers/coursesRouter");
@@ -54,6 +57,36 @@ app.use("/users", userRouter);
 app.use("/courses", courseRouter);
 app.use("/forums", forumRouter);
 
-app.listen(PORT, () => {
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3001",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User ${socket.id} joined room: ${data}`);
+  });
+
+  socket.on("send_message", async (data) => {
+    console.log(data);
+    // calling createNewMessage from messengerController to add data into database
+    // let newMessage = await messengerController.createNewMessage(data);
+    // let newMessage = await message.create(data);
+
+    // emit message into specific chatroom
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
+
+server.listen(PORT, () => {
   console.log(`Express app listening on port ${PORT}!`);
 });
