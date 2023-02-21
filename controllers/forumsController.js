6249afed4efe7762d3d0e4e9b6eb50e513aad861
yuj8fuj6 <yuj.m.fuj91@gmail.com>
2023-02-22 02:37:22 +0800

    Done backend filtering for courses, for forum page

--- controllers/forumsController.js
+++ controllers/forumsController.js
@@ -1,5 +1,7 @@
 const BaseController = require("./baseController");
 
+const { Op } = require("sequelize");
+
 class ForumsController extends BaseController {
   constructor(model, courseModel, postModel, adminForumModel) {
     super(model);
@@ -23,6 +25,32 @@ class ForumsController extends BaseController {
     try {
       const allForumAdmins = await this.adminForumModel.findAll();
       return res.json(allForumAdmins);
+    } catch (err) {
+      return res.status(400).json({ error: true, msg: err });
+    }
+  }
+
+  // async addOne (req, res){
+  //   try {
+  //     // const newForum = await this.model
+  //   } catch (err) {
+  //     return res.status(400).json({ error: true, msg: err });
+  //   }
+  // }
+
+  async filterCourses(req, res) {
+    try {
+      const allForums = await this.model.findAll({
+        include: [{ model: this.courseModel }],
+      });
+      const forumCourseCode = [];
+      allForums.forEach((forum) => {
+        forumCourseCode.push(forum.course.course_code);
+      });
+      const filteredCourses = await this.courseModel.findAll({
+        where: { course_code: { [Op.notIn]: forumCourseCode } },
+      });
+      return res.json(filteredCourses);
     } catch (err) {
       return res.status(400).json({ error: true, msg: err });
     }
--- routers/forumsRouter.js
+++ routers/forumsRouter.js
@@ -8,7 +8,14 @@ class ForumsRouter {
   routes() {
     // we will insert routes into here later on
     router.get("/", this.controller.getAll.bind(this.controller));
-    router.get("/all_admins", this.controller.getAllForumAdmins.bind(this.controller));
+    router.get(
+      "/all_admins",
+      this.controller.getAllForumAdmins.bind(this.controller),
+    );
+    router.get(
+      "/filteredCourses",
+      this.controller.filterCourses.bind(this.controller),
+    );
     return router;
   }
 }
