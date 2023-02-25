afb15e6109f57ca8cd226d8bbe8f4607315ec2c4
LoyChaiEe <celoy001@gmail.com>
2023-02-25 10:56:28 +0800

    corrected merge conflict

--- db/models/student.js
+++ db/models/student.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       this.belongsTo(models.user);
       this.hasMany(models.post);
-      this.hasMany(models.post_upvote);
+      this.hasMany(models.postUpvote);
       this.belongsToMany(models.course_indice, {
         through: "course_registrations",
       });
